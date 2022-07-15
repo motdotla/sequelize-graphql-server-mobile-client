0cd870ef350a9ebd9aff63557f98dbb4214994c6
usmansbk <usmansbk@gmail.com>
2022-07-15 15:46:24 +0100

    setup linters

---
+++ src/config/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "react-native-paper";
+import { Theme } from "react-native-paper/lib/typescript/types";
+
+const theme: Theme = {
+  ...DefaultTheme,
+};
+
+export default theme;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { Text, Provider as PaperProvider } from "react-native-paper";
+import theme from "./config/theme";
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <Text>My App</Text>
+    </PaperProvider>
+  );
+}
