360c717dcd2a5d6e928b91fb6084717da47d0fe7
usmansbk <usmansbk@gmail.com>
2022-07-16 18:46:08 +0100

    nit

--- src/screens/auth/GoogleLogin.tsx
+++ src/screens/auth/GoogleLogin.tsx
@@ -4,7 +4,12 @@ import { Button } from "react-native-paper";
 export default function GoogleLogin() {
   const { t } = useTranslation();
   return (
-    <Button mode="contained-tonal" buttonColor="#4285F4" textColor="#fff">
+    <Button
+      mode="contained-tonal"
+      buttonColor="#4285F4"
+      textColor="#fff"
+      onPress={() => null}
+    >
       {t("Sign in with Google")}
     </Button>
   );
