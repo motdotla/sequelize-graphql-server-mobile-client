557b8e9c1cd145b56f24d7785e7ce4d1b1be35ea
usmansbk <usmansbk@gmail.com>
2022-07-19 21:04:33 +0100

    add forgot and reset password

--- src/screens/auth/ForgotPassword.tsx
+++ src/screens/auth/ForgotPassword.tsx
@@ -2,15 +2,19 @@ import { useMemo } from "react";
 import { StyleSheet, View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { ScrollView } from "react-native-gesture-handler";
-import { TextInput, HelperText } from "react-native-paper";
+import { TextInput, HelperText, Banner } from "react-native-paper";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Button from "~components/Button";
 import * as yup from "yup";
-import { ForgotPasswordInput } from "types";
+import { ForgotPasswordInput, RootStackScreenProps } from "types";
+import { useForgotPassword } from "~hooks/api/forgotPassword";
 
-export default function ForgotPassword() {
+export default function ForgotPassword({
+  navigation,
+}: RootStackScreenProps<"ForgotPassword">) {
   const { t } = useTranslation();
+  const { loading, onSubmit, data } = useForgotPassword();
 
   const schema = useMemo(
     () =>
@@ -37,40 +41,60 @@ export default function ForgotPassword() {
   });
 
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="always"
-      contentContainerStyle={styles.contentContainer}
-    >
-      <Controller
-        name="email"
-        control={control}
-        render={({ field: { onBlur, onChange, value } }) => (
-          <View style={styles.gap}>
-            <TextInput
-              autoFocus
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              autoComplete="email"
-              label={t("Email")}
-              keyboardType="email-address"
-              error={touchedFields.email && !!errors.email}
-            />
-            {touchedFields.email && !!errors.email && (
-              <HelperText
-                visible={touchedFields.email && !!errors.email}
-                type="error"
-              >
-                {errors.email?.message}
-              </HelperText>
-            )}
-          </View>
-        )}
-      />
-      <Button mode="contained" onPress={handleSubmit(console.log)}>
-        {t("Continue")}
-      </Button>
-    </ScrollView>
+    <>
+      {!!data && (
+        <Banner
+          visible
+          actions={[
+            {
+              label: t("Back to Login"),
+              onPress: () => navigation.navigate("Login"),
+            },
+          ]}
+        >
+          {data.message}
+        </Banner>
+      )}
+      <ScrollView
+        keyboardShouldPersistTaps="always"
+        contentContainerStyle={styles.contentContainer}
+      >
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <View style={styles.gap}>
+              <TextInput
+                autoFocus
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                autoComplete="email"
+                label={t("Email")}
+                keyboardType="email-address"
+                error={touchedFields.email && !!errors.email}
+              />
+              {touchedFields.email && !!errors.email && (
+                <HelperText
+                  visible={touchedFields.email && !!errors.email}
+                  type="error"
+                >
+                  {errors.email?.message}
+                </HelperText>
+              )}
+            </View>
+          )}
+        />
+        <Button
+          mode="contained"
+          onPress={handleSubmit(onSubmit)}
+          loading={loading}
+          disabled={loading}
+        >
+          {t("Continue")}
+        </Button>
+      </ScrollView>
+    </>
   );
 }
 
--- src/screens/auth/ResetPassword.tsx
+++ src/screens/auth/ResetPassword.tsx
@@ -2,16 +2,22 @@ import { useMemo, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { ScrollView } from "react-native-gesture-handler";
-import { TextInput, HelperText } from "react-native-paper";
+import { TextInput, HelperText, Banner } from "react-native-paper";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Button from "~components/Button";
-import { ResetPasswordInput } from "types";
+import { ResetPasswordInput, RootStackScreenProps } from "types";
+import { useResetPassword } from "~hooks/api/forgotPassword";
 
-export default function ResetPassword() {
+export default function ResetPassword({
+  route,
+  navigation,
+}: RootStackScreenProps<"ResetPassword">) {
   const { t } = useTranslation();
   const [secureTextEntry, setSecureTextEntry] = useState(true);
+  const { loading, data, onSubmit } = useResetPassword();
+  const token = route.params?.token;
 
   const schema = useMemo(
     () =>
@@ -21,6 +27,7 @@ export default function ResetPassword() {
             .string()
             .required(t("Type your new password"))
             .min(6, t("Password must be at least 6 characters long")),
+          token: yup.string().required(t("The link you followed is broken.")),
         })
         .required(),
     [t]
@@ -34,50 +41,76 @@ export default function ResetPassword() {
     resolver: yupResolver(schema),
     defaultValues: {
       password: "",
+      token,
     },
   });
 
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="always"
-      contentContainerStyle={styles.contentContainer}
-    >
-      <Controller
-        name="password"
-        control={control}
-        render={({ field: { value, onBlur, onChange } }) => (
-          <View style={styles.gap}>
-            <TextInput
-              autoFocus
-              value={value}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              secureTextEntry={secureTextEntry}
-              label={t("New Password")}
-              autoComplete="password-new"
-              right={
-                <TextInput.Icon
-                  name={secureTextEntry ? "eye" : "eye-off"}
-                  onPress={() => setSecureTextEntry(!secureTextEntry)}
-                />
-              }
-              error={touchedFields.password && !!errors.password}
-            />
-            {touchedFields.password && !!errors.password && (
-              <HelperText
-                visible={touchedFields.password && !!errors.password}
-                type="error"
-              >
-                {errors.password?.message}
-              </HelperText>
-            )}
-          </View>
-        )}
-      />
-      <Button mode="contained" onPress={handleSubmit(console.log)}>
-        {t("Change my password")}
-      </Button>
-    </ScrollView>
+    <>
+      {data && (
+        <Banner
+          visible
+          actions={[
+            {
+              label: t("Back to Login"),
+              onPress: () => navigation.navigate("Login"),
+            },
+          ]}
+        >
+          {data.message}
+        </Banner>
+      )}
+      <ScrollView
+        keyboardShouldPersistTaps="always"
+        contentContainerStyle={styles.contentContainer}
+      >
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => (
+            <View style={styles.gap}>
+              <TextInput
+                autoFocus
+                value={value}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                secureTextEntry={secureTextEntry}
+                label={t("New Password")}
+                autoComplete="password-new"
+                right={
+                  <TextInput.Icon
+                    name={secureTextEntry ? "eye" : "eye-off"}
+                    onPress={() => setSecureTextEntry(!secureTextEntry)}
+                  />
+                }
+                error={touchedFields.password && !!errors.password}
+              />
+              {touchedFields.password && !!errors.password && (
+                <HelperText
+                  visible={touchedFields.password && !!errors.password}
+                  type="error"
+                >
+                  {errors.password?.message}
+                </HelperText>
+              )}
+              {!!errors.token && (
+                <HelperText visible={!!errors.token} type="error">
+                  {errors.token?.message}
+                </HelperText>
+              )}
+            </View>
+          )}
+        />
+        <Button
+          mode="contained"
+          onPress={handleSubmit(onSubmit)}
+          loading={loading}
+          disabled={loading}
+        >
+          {t("Change my password")}
+        </Button>
+      </ScrollView>
+    </>
   );
 }
 
